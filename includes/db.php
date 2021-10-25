ff954a92cc2b7477a470a43f2c95046c9123dc0a
Samuel Addisu <samueladdisu7@gmail.com>
2021-10-25 17:18:48 +0300

    db.php fix

--- includes/db.php
+++ includes/db.php
@@ -1,7 +1,12 @@
 <?php
-$connection = mysqli_connect('localhost','root','','proclamations');
+$connection = mysqli_connect('localhost','fikirlawcom_Sam','8+(B^=e3eKu$','fikirlawcom_proclamation');
 
-
+if (!$connection) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
 
 
 
